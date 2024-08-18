45d41a6ba65182946afa32b20c4c4158bbe74eb0
Hoang <114314239+Gink-Hoang@users.noreply.github.com>
2024-08-18 09:59:35 +0700

    first commit

---
+++ routes/User.js
@@ -0,0 +1,38 @@
+// Import the required modules
+const express = require("express")
+const router = express.Router()
+
+// Import the required controllers and middleware functions
+const {
+  login,
+  signup,
+  sendotp,
+  changePassword,
+} = require("../controllers/Auth")
+const {
+  resetPasswordToken,
+  resetPassword,
+} = require("../controllers/ResetPassword")
+
+const { auth } = require("../middlewares/auth")
+router.post("/login", login)
+
+router.post("/signup", signup)
+
+router.post("/sendotp", sendotp)
+
+// Route for Changing the password
+router.post("/changepassword", auth, changePassword)
+
+// ********************************************************************************************************
+//                                      Reset Password
+// ********************************************************************************************************
+
+// Route for generating a reset password token
+router.post("/reset-password-token", resetPasswordToken)
+
+// Route for resetting user's password after verification
+router.post("/reset-password", resetPassword)
+
+// Export the router for use in the main application
+module.exports = router
